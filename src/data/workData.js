00b3f51afe03262b0e9f0702954778d018b89d5e
Atta Navaid <a302@umbc.edu>
2024-09-08 04:27:55 -0400

    add learnprompting

--- src/data/workData.js
+++ src/data/workData.js
@@ -1,7 +1,26 @@
 export const workData = [
     {
+        "icon": require("../assets/work/lp2.png"),
+        "title": "Learn Prompting Company, INC.",
+        "subtitle": "Fullstack Developer",
+        "period": "April 2024 - Present",
+        "location": "College Park, Maryland, United States",
+        "description": 
+            <p>
+                Implement/Update pages, functionalities, tools, components, etc. using React, TypeScript, Tailwind, and MDX.
+                <br/>
+                Make active contributions to site’s GitHub page via pull requests and resolve issues and conflicts on site’s GitHub.
+                <br/>
+                Resolve site bugs and errors using debugging tools on company’s website:&nbsp; 
+                <a href="https://learnprompting.org" target="_blank" rel="noopener noreferrer">learnprompting.org</a>.
+                <br/>
+                <br/>
+            </p>,
+        "specialization": "NextJS, React, Typescript, Markdown, Tailwind CSS"
+    },
+    {
         "icon": require("../assets/work/3d.png"),
-        "title": "Self-Employed Online",
+        "title": "ModelsByAT",
         "subtitle": "3D Asset Seller",
         "period": "August 2019 - Present",
         "location": "Catonsville, Maryland, United States",
